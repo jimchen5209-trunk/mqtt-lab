2aca03a5dddc22d24a3fa103714116f867d52a35
jimchen5209 <jimchen5209@gmail.com>
2022-07-13 14:11:39 +0800

    fix react git

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -57,7 +57,7 @@ const App = () => {
   }, [chat]);
 
   const onEnterPress = (e) => {
-    if (e.keyCode == 13 && e.shiftKey == false) {
+    if (e.keyCode === 13 && e.shiftKey === false) {
       e.preventDefault();
       sendMessage();
     }
