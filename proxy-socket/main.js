06b4e7c26ea1508571b542fc3f1172c46dec97a8
jimchen5209 <jimchen5209@gmail.com>
2022-07-13 14:13:32 +0800

    socket

--- proxy-socket/main.js
+++ proxy-socket/main.js
@@ -29,18 +29,34 @@ server.listen(serverPort, () => {
 mqtt init
 */ 
 
+// mqtt 的基本設定
+const client = mqtt.connect("mqtt://localhost:1883", {
+  username: "admin",
+  password: "admin",
+});
+
+// 建立 mqtt 連線並 subscribe topic 
+client.on("connect", function () {
+  console.log("MQTT CONNECTION START");
+  client.subscribe("monosparta/chat");
+});
+
 const io = require("socket.io")(server);
 
 io.on("connection", function (socket) {
-  socket.on("disconnect", function () {
-    
-  });
+  socket.on("disconnect", function () {});
   socket.on("chat", function (msg) {
-    //
+    // 從 client 收到訊息時，使用 topic 傳送訊息到 mqtt broker
+    client.publish("monosparta/chat", JSON.stringify(msg));
   });
-  try {
-    //
-  } catch {
-    socket.emit("data", "Node not found.");
-  }
+
+  // 從 mqtt broker 收到訊息時
+  client.on("message", function (topic, msg) {
+    try {
+      // 以 test 發送訊息給監聽的 client
+      if (topic === "monosparta/chat") socket.emit("chat", JSON.parse(msg));
+    } catch {
+      socket.emit("data", "Node not found.");
+    }
+  });
 });
